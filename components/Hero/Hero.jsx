7bea807b24f819157350e6f62831c45854ab6365
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2022-12-24 02:00:56 +0800

    create bottom section on the hero

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import styles from "../../styles/Hero.module.css";
 import arrowRight from "../../public/arrow-right.svg";
 import arrowBg from "../../public/Arrow_bg.svg";
+import mouse from "../../public/mouse-solid.svg";
+import star from "../../public/star.svg";
 
 function Hero() {
   return (
@@ -14,15 +16,24 @@ function Hero() {
         Enovate is an agency that specializes in design and development of
         websites, web applications and mobile apps.
       </h2>
-      <div className={styles["bg_arrow"]}>
-        <Image src={arrowBg} alt="arrow_bg" />
-      </div>
       <button className={styles["hero-btn"]}>
         Let&#39;s create something together
         <span className={styles["arrow-container"]}>
           <Image src={arrowRight} alt="arrow-right" />
         </span>
       </button>
+      <div className={styles["hero-bottom"]}>
+        <div className={styles["bottom-text"]}>
+          <p className={styles["get-fascinated"]}>
+            {" "}
+            Scroll down and get fascinated
+          </p>
+          <Image src={mouse} alt="mouse_svg" />
+        </div>
+        <div className={styles["star-container"]}>
+          <Image src={star} alt="star" />
+        </div>
+      </div>
     </div>
   );
 }
