ef88e24caf60ab9ba894883e394226e4c36d443a
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2022-12-24 01:02:02 +0800

    create hero section

---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+import styles from "../../styles/Hero.module.css";
+import arrowRight from "../../public/arrow-right.svg";
+import arrowBg from "../../public/Arrow_bg.svg";
+
+function Hero() {
+  return (
+    <div className={styles["hero-container"]}>
+      <h4 className={styles["hero-title"]}>
+        We create stunning and timeless experiences
+      </h4>
+      <h2 className={styles["hero-subtitle"]}>
+        Enovate is an agency that specializes in design and development of
+        websites, web applications and mobile apps.
+      </h2>
+      <div className={styles["bg_arrow"]}>
+        <Image src={arrowBg} alt="arrow_bg" />
+      </div>
+      <button className={styles["hero-btn"]}>
+        Let&#39;s create something together
+        <span className={styles["arrow-container"]}>
+          <Image src={arrowRight} alt="arrow-right" />
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function Home() {
         <link rel="icon" href="/logo.ico" />
       </Head>
       <main>
-        {/* Header */}
+        {/* Header-section */}
         <Header />
+        {/* Hero-section */}
+        <Hero />
       </main>
     </>
   );
